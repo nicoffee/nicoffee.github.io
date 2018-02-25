13ccdc4031ed7072104b9cc8bf2473576cb144d3
nicoffee <fedoseevn@gmail.com>
2018-02-25 23:10:50 +0500

    Add production build

--- app/index.js
+++ app/index.js
@@ -1,24 +1,24 @@
-const fetchData = (url, headers) =>
-  fetch(url, {
-    headers: {
-      Accept: 'application/vnd.github.v3+json',
-      ...headers
-    },
-    method: 'GET'
-  }).then(res => res.json());
+// const fetchData = (url, headers) =>
+//   fetch(url, {
+//     headers: {
+//       Accept: 'application/vnd.github.v3+json',
+//       ...headers
+//     },
+//     method: 'GET'
+//   }).then(res => res.json());
 
-const render = element => console.log(element);
+// const render = element => console.log(element);
 
-fetchData('https://api.github.com/users/nicoffee/repos')
-  .then(repos => {
-    console.log('repos', repos);
-    repos.forEach(repo => {
-      console.log('repo', repo);
-      fetchData(`https://api.github.com/repos/nicoffee/${repo.name}/topics`, {
-        Accept: 'application/vnd.github.mercy-preview+json'
-      }).then(tags => {
-        console.log('tags', tags);
-      });
-    });
-  })
-  .catch(err => render(err));
+// fetchData('https://api.github.com/users/nicoffee/repos')
+//   .then(repos => {
+//     console.log('repos', repos);
+//     repos.forEach(repo => {
+//       console.log('repo', repo);
+//       fetchData(`https://api.github.com/repos/nicoffee/${repo.name}/topics`, {
+//         Accept: 'application/vnd.github.mercy-preview+json'
+//       }).then(tags => {
+//         console.log('tags', tags);
+//       });
+//     });
+//   })
+//   .catch(err => render(err));
